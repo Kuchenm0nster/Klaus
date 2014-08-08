8ee68cecce581cb1f61570fdd9714b8db0207c8e
Florian Bogenhard <florian.bogenhard@mni.thm.de>
2014-08-08 23:48:07 +0200

    fügt den btn_link_to helper hinzu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def btn_link_to(name, path, *args)
+    options = args.extract_options!
+    html_class = "btn " + (options.delete(:class) || 'btn-default')
+    icon = options.delete(:icon)
+
+    if icon
+      if icon.is_a? Hash
+        icon_class = icon[:class]
+        icon = icon[:icon]
+      end
+      span = content_tag("span", nil, class: "glyphicon glyphicon-#{icon} #{icon_class}") if icon
+
+      if name && !name.empty?
+        name = "#{span} #{name}".html_safe
+      else
+        name = "#{span}".html_safe
+      end
+    end
+
+    link_to name, path, {class: html_class}.update(options)
+  end
 end
